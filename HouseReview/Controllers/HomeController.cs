3fc3b3cfca1cd9824bf2c714157fa81741960ad1
unknown <moirted@gmail.com>
2022-05-06 19:21:28 +0300

    Region map changes

--- HouseReview/Controllers/HomeController.cs
+++ HouseReview/Controllers/HomeController.cs
@@ -2,22 +2,37 @@
 using HouseReview.Models;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using HouseReview.Areas.Identity.Data;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HouseReview.Controllers
 {
     [Authorize]
     public class HomeController:Controller
     {
-		//[HttpGet]
-  //      public IActionResult CreateUser() => View();
-		//[HttpPost]
-  //      public async Task<IActionResult> CreateUser(CreateUser model) {
-  //          if (ModelState.IsValid)
-  //          {
-  //              return RedirectToAction("Index");
-  //          }
-  //          else return View();
-		//}
+        //[HttpGet]
+        //      public IActionResult CreateUser() => View();
+        //[HttpPost]
+        //      public async Task<IActionResult> CreateUser(CreateUser model) {
+        //          if (ModelState.IsValid)
+        //          {
+        //              return RedirectToAction("Index");
+        //          }
+        //          else return View();
+        //}
+        private readonly CommentsDBContext _db;
+        public HomeController(CommentsDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Comment()
+        {
+            IEnumerable<Comment> objList = _db.Comments;
+            return View(objList);
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -40,42 +55,50 @@ namespace HouseReview.Controllers
 
         public IActionResult Firstm()
         {
-            return View();
+            IEnumerable<Comment> objList = _db.Comments.Where(x => (x.Adress == "Первомайский"));
+            return View(objList);
         }
 
         public IActionResult Kirov()
         {
-            return View();
+            IEnumerable<Comment> objList = _db.Comments.Where(x => (x.Adress == "Кировский"));
+            return View(objList);
         }
 
         public IActionResult Lenin()
         {
-            return View();
+            IEnumerable<Comment> objList = _db.Comments.Where(x => (x.Adress == "Ленинский"));
+            return View(objList);
         }
 
         public IActionResult October()
         {
-            return View();
+            IEnumerable<Comment> objList = _db.Comments.Where(x => (x.Adress == "Октябрьский"));
+            return View(objList);
         }
 
         public IActionResult Railw()
         {
-            return View();
+            IEnumerable<Comment> objList = _db.Comments.Where(x => (x.Adress == "Железнодорожный"));
+            return View(objList);
         }
 
         public IActionResult Soviet()
         {
-            return View();
+            IEnumerable<Comment> objList = _db.Comments.Where(x => (x.Adress == "Советский"));
+            return View(objList);
         }
 
         public IActionResult Voroshil()
         {
-            return View();
+            IEnumerable<Comment> objList = _db.Comments.Where(x => (x.Adress == "Ворошиловский"));
+            return View(objList);
         }
 
         public IActionResult Prolet()
         {
-            return View();
+            IEnumerable<Comment> objList = _db.Comments.Where(x => (x.Adress == "Пролетарский"));
+            return View(objList);
         }
     }
 }
